5b5737f47238b7ea8f81eaaaa2d690ab1de42b53
treguv <vladislavtregubov00@gmail.com>
2020-12-08 14:03:43 -0800

    added get and delete routes

--- server.js
+++ server.js
@@ -13,6 +13,58 @@ const db = new sqlite3.Database("./db/election.db", (err) => {
 
   console.log("Connected to election database");
 });
+
+//get all candidates
+app.get("/api/candidates", (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+//get single candidate
+app.get("/api/candidates/:id", (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+// Delete a candidate
+app.delete("/api/candidate/:id", (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({
+      message: "successfully deleted",
+      changes: this.changes,
+    });
+  });
+});
+
 //catch all invalid requests not supported by the server
 app.use((req, res) => {
   res.status(404).end();
