3cf45dc0b83125bb5f80ffbfd5925c17f07f6354
treguv <vladislavtregubov00@gmail.com>
2020-12-08 14:34:01 -0800

    Added post route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const sqlite3 = require("sqlite3").verbose();
+const inputCheck = require("./utils/inputCheck");
 //add midleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -65,6 +66,35 @@ app.delete("/api/candidate/:id", (req, res) => {
   });
 });
 
+//post route to add into db
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  //if we are don here that means the data is valid
+
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+  VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+      id: this.lastID,
+    });
+  });
+});
 //catch all invalid requests not supported by the server
 app.use((req, res) => {
   res.status(404).end();
